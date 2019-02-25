240acf33aad9742824914d016f12d03daabec576
leo <leo@otrusa.com>
2019-02-25 14:08:55 -0500

    Icon

--- otrusa_app/__manifest__.py
+++ otrusa_app/__manifest__.py
@@ -1,37 +1,31 @@
 # -*- coding: utf-8 -*-
 {
     'name': "otrusa_app",
-
-    'summary': """
-        Custom app by OTRUSA.COM""",
-
-    'description': """
-        This app contain modification of view for OTRUSA.COM
-    """,
-
+    'version': 'beta 1.0',
+    'category': 'Warehouse',
+    'sequence': 1,
+    'summary':
+        """
+            Custom app by OTRUSA.COM
+        """,
+    'description': 
+        """
+            This app contain modification of view for OTRUSA.COM
+        """,
     'author': "OTRUSA.COM",
     'website': "http://www.otrusa.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
-    # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
     'images': ['images/main_screenshot.png'],
-    # any module necessary for this one to work correctly
-    'depends': ['inventory','sale','account'],
-
-    # always loaded
+    'depends': ['stock','sale','account'], # any module necessary for this one to work correctly
     'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
-    ],
+                # 'security/ir.model.access.csv',
+                'views/views.xml',
+                'views/templates.xml',
+            ],
     'installable': True,
     'auto_install': False,
     'application': True,
     # only loaded in demonstration mode
     'demo': [
-        'demo/demo.xml',
-    ],
+                'demo.xml',
+            ],
 }
