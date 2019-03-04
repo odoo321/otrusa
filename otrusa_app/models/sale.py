3c7457b2ec675c2a75b94bc1bce9c69b9816cbef
leo <leo@otrusa.com>
2019-03-04 18:11:25 -0500

    22

--- otrusa_app/models/sale.py
+++ otrusa_app/models/sale.py
@@ -17,3 +17,16 @@ from odoo import models, fields, api
 class sale_order(models.Model):
     _inherit= 'sale.order'
     x_test_otrusa = fields.Float('test_otrusa')
+
+    @api.multi
+    def test01(self):
+        invoices = self.mapped('invoice_ids')
+        action = self.env.ref('account.action_invoice_tree1').read()[0]
+        if len(invoices) > 1:
+            action['domain'] = [('id', 'in', invoices.ids)]
+        elif len(invoices) == 1:
+            action['views'] = [(self.env.ref('account.invoice_form').id, 'form')]
+            action['res_id'] = invoices.ids[0]
+        else:
+            action = {'type': 'ir.actions.act_window_close'}
+        return action
