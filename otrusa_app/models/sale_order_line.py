927c1a8553e52846808b53bb601ab79707e0b777
leo <leo@otrusa.com>
2019-03-04 14:46:49 -0500

    03

--- otrusa_app/models/sale_order_line.py
+++ otrusa_app/models/sale_order_line.py
@@ -10,5 +10,10 @@ from odoo import models, fields, api
 
 class sale_order_line(models.Model):
     _inherit= 'sale.order.line'
-
     x_test_app = fields.Float('x_test_app')
+
+@api.multi
+def x_set_dropship_route_id(self):
+    for order in self:
+        test = 6
+        order.order_line.route_id = 6
