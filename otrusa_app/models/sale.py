8c8006fdafaaba5f3847f8132e1f16161538d127
leo <leo@otrusa.com>
2019-03-04 20:30:27 -0500

    31

--- otrusa_app/models/sale.py
+++ otrusa_app/models/sale.py
@@ -8,18 +8,5 @@ class saleOrder(models.Model):
 
     @api.multi
     def test01(self):
-        invoices = self.mapped('invoice_ids')
-        action = self.env.ref('account.action_invoice_tree1').read()[0]
-        if len(invoices) > 1:
-            action['domain'] = [('id', 'in', invoices.ids)]
-        elif len(invoices) == 1:
-            action['views'] = [(self.env.ref('account.invoice_form').id, 'form')]
-            action['res_id'] = invoices.ids[0]
-        else:
-            action = {'type': 'ir.actions.act_window_close'}
-        return action
-
-
-
-class SaleOrderLine(models.Model):
-    _inherit = "sale.order.line"
+        for order in self:
+            order.order_line.update({'x_description': order.x_internal_note})
